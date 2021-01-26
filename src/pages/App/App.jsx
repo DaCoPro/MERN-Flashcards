b64bd8576247cda947a2c526ea43c28dd7ade019
DaCoPro <comerbdavid@gmail.com>
2021-01-25 19:27:57 -0800

    Add page level components.

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import './App.css';
 
 export default function App() {
+  const [user, setUser] = useState(null);
   return (
     <main className="App">
       App
---
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -0,0 +1,3 @@
+export default function OrderHistoryPage() {
+    return <h1>OrderHistoryPage</h1>
+}
