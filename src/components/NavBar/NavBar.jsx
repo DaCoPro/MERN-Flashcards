c303db4d2485807300b1da64a0cf41737df73194
DaCoPro <comerbdavid@gmail.com>
2021-01-27 14:42:24 -0800

    Finish styling nav dropdown. Style Nav.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,33 +1,27 @@
 import './NavBar.css';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import * as userService from '../../utilities/users-service';
+
 import NavItems from '../NavItems/NavItems';
 
-export default function NavBar({ user, setUser }) {
+export default function NavBar({ setUser }) {
+  //state and click handler for showing nav items
   const [showItems, setShowItems] = useState(-1);
+  const handleBurgerClick = () => setShowItems(showItems * -1)
   
-  function handleLogOut() {
-    userService.logOut();
-    setUser(null);
-  }
-  function handleBurgerClick() {
-    return setShowItems(showItems * -1);
-  } 
-  // const handleBurgerClick = () => setShowItems(true)
+  
+  
   return (
     <>
       <nav className="NavBar">
-        <Link to="/home">Welcome</Link>
+        <Link to="/home" className="Logo">Logo</Link>
         <div className="container" onClick={handleBurgerClick}>
           <div className="bar1"></div>
           <div className="bar2"></div>
           <div className="bar3"></div>
         </div>
-        
-        <Link to="" onClick={handleLogOut}>Log Out</Link>
       </nav>
-      { showItems > 0 ? <NavItems /> : null }
+      { showItems > 0 ? <NavItems setUser={setUser} /> : null }
     </>
 
   );
--- src/components/NavItems/NavItems.jsx
+++ src/components/NavItems/NavItems.jsx
@@ -1,10 +1,17 @@
+import './NavItems.css';
+import * as userService from '../../utilities/users-service';
 import { Link } from 'react-router-dom';
 
-export default function NavItems () {
+export default function NavItems ({ setUser }) {
+    function handleLogOut() {
+        userService.logOut();
+        setUser(null);
+      }
     return(
-        <>
-            <Link to="/decks">Decks</Link>
-            <Link to="/study">Study</Link>
-        </>
+        <div className="NavItems">
+            <Link to="/decks" className="Item">Decks</Link>
+            <Link to="/study" className="Item">Study</Link>
+            <Link to="" onClick={handleLogOut} className="Item">Log Out</Link>
+        </div>
     );
 }
